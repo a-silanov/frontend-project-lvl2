6e1bbde070dc4d473b95979a791ab418031e9625
Alexander Silanov <silanov.alexander@gmail.com>
2022-09-07 21:34:02 +0200

    delete

--- __tests__/index.test.js
+++
@@ -1,6 +0,0 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-import { readFileSync } from 'node:fs';
-import genDiff from '../src/index.js';
-
-const __filename = fileURLToPath(import.meta.url);
